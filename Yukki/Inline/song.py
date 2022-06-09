8dd6e798dca2a14415c98d8dd02c7ee40e7d32a6
ꜱᴏᴜᴛʜᴋɪʟʟᴀ <91430346+GzForeign@users.noreply.github.com>
2022-06-09 19:18:07 +0700

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -10,7 +10,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
                 callback_data=f"song_right B|{query_type}|{query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="Download",
+                text="Unduh",
                 callback_data=f"qwertyuiopasdfghjkl {videoid}|{user_id}",
             ),
             InlineKeyboardButton(
@@ -20,7 +20,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Tutup Pencarian",
+                text="🗑 Tutup",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -32,11 +32,11 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Dapatkan Audio",
+                text="⬇️ Ambil Audio",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Dapatkan Video",
+                text="⬇️ Ambil Video",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
