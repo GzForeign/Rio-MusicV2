6ee32e33f7d1bc3b7c2f5e545b2297fe20aa68ae
ꜱᴏᴜᴛʜᴋɪʟʟᴀ <91430346+GzForeign@users.noreply.github.com>
2022-06-09 19:05:40 +0700

    Update gen_session.py

--- gen_session.py
+++ gen_session.py
@@ -2,8 +2,8 @@ import asyncio
 
 from pyrogram import Client as c
 
-API_ID = input("\nEnter Your API_ID:\n > ")
-API_HASH = input("\nEnter Your API_HASH:\n > ")
+API_ID = input("\nEnter Ur API_ID:\n > ")
+API_HASH = input("\nEnter Ur API_HASH:\n > ")
 
 print("\n\n Enter Phone number when asked.\n\n")
 
@@ -13,7 +13,7 @@ i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 async def main():
     await i.start()
     ss = await i.export_session_string()
-    print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n")
+    print("\nHERE IS UR STRING SESSION, COPY IT, DON'T SHARE!!\n")
     print(f"\n{ss}\n")
 
 
